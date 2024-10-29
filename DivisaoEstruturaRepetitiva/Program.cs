67ce92c4a61c9e836d9f343818c99362375a5983
Gabriela <gabriela.priaro@gmail.com>
2024-10-29 17:27:33 +0000

    Implementaçao para calcular divisao entre dois numeros e mostrar mensagem de aviso, caso nao for possivel.

--- DivisaoEstruturaRepetitiva/Program.cs
+++ DivisaoEstruturaRepetitiva/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace DivisaoEstruturaRepetitiva
+﻿//obs: Utilizando estrutura repetitiva For:
+//Problema "divisao" (adaptado de URI 1116)
+//Escreva um algoritmo que leia dois números e imprima o resultado da divisão do primeiro pelo
+//segundo. Caso não for possível, mostre a mensagem “DIVISAO IMPOSSIVEL”. 
+
+using System.Globalization;
+
+namespace DivisaoEstruturaRepetitiva
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n, numerador, denominador;
+            double divisao;
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Quantos casos serõ digitados? ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Digite o numerador: ");
+                numerador = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o denominador: ");
+                denominador = int.Parse(Console.ReadLine());
+
+                divisao = (double)numerador / denominador;
+
+                if (denominador == 0)
+                {
+                    Console.WriteLine("DIVISAO IMPOSSIVEL");
+                }
+                else
+                {
+                    Console.WriteLine($"DIVISAO = {divisao.ToString("F2", CI)}");
+                }
+            }
         }
     }
 }
